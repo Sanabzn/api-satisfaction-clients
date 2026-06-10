fccc5fb37a07e46dbf546076dc96ab42a5338bbb
Sanabzn <sanabouziane.archi@gmail.com>
2026-06-10 14:24:16 +0000

    Fix Cloud Build API tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,47 +1,40 @@
-import sys
-import os
+import requests
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from fastapi.testclient import TestClient
-import main
-
-client = TestClient(main.app)
+API_URL = "https://api-satisfaction-913270433936.europe-west1.run.app"
 
 
-def test_root():
-    response = client.get("/")
+def test_root_endpoint():
+    response = requests.get(API_URL)
+
     assert response.status_code == 200
     assert "message" in response.json()
 
 
-def test_predict_positive(monkeypatch):
-    monkeypatch.setattr(main, "save_prediction", lambda text, sentiment, score: None)
-
-    response = client.post(
-        "/predict",
+def test_predict_positive():
+    response = requests.post(
+        f"{API_URL}/predict",
         json={"text": "service excellent"}
     )
 
     assert response.status_code == 200
 
     data = response.json()
+
     assert "sentiment" in data
     assert "score" in data
     assert "text" in data
 
 
-def test_predict_negative(monkeypatch):
-    monkeypatch.setattr(main, "save_prediction", lambda text, sentiment, score: None)
-
-    response = client.post(
-        "/predict",
+def test_predict_negative():
+    response = requests.post(
+        f"{API_URL}/predict",
         json={"text": "service très mauvais"}
     )
 
     assert response.status_code == 200
 
     data = response.json()
+
     assert "sentiment" in data
     assert "score" in data
     assert "text" in data
